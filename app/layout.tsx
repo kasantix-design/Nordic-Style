b69fefedc7e8ed3485444d4ef732826db722f949
kasantix-design <kasantix@gmail.com>
2026-06-01 11:19:33 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import { CartProvider } from "components/cart/cart-context";
 import { Navbar } from "components/layout/navbar";
-import { WelcomeToast } from "components/welcome-toast";
 import { GeistSans } from "geist/font/sans";
 import { ReactNode } from "react";
 import { Toaster } from "sonner";
@@ -33,7 +32,7 @@ export default function RootLayout({
           <main>
             {children}
             <Toaster closeButton />
-            <WelcomeToast />
+            {/* <WelcomeToast /> er fjernet siden filen er slettet */}
           </main>
         </CartProvider>
       </body>
