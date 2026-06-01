9e52d03ce268979234d502d842c0ca10d2312cec
kasantix-design <kasantix@gmail.com>
2026-06-01 15:29:40 +0200

    Create RootLayout component with metadata and providers
    Set up the root layout with metadata, cart context, and navbar.

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import { CartProvider } from "components/cart/cart-context";
+import { Navbar } from "components/layout/navbar";
+import { GeistSans } from "geist/font/sans";
+import { ReactNode } from "react";
+import { Toaster } from "sonner";
+import "./globals.css";
+
+export const metadata = {
+  title: {
+    default: "Nordic Style",
+    template: "%s | Nordic Style",
+  },
+  robots: {
+    follow: true,
+    index: true,
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const cartPromise = Promise.resolve(null);
+
+  return (
+    <html lang="no" className={GeistSans.variable}>
+      <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
+        <CartProvider cartPromise={cartPromise}>
+          <Navbar />
+          <main>
+            {children}
+            <Toaster closeButton />
+          </main>
+        </CartProvider>
+      </body>
+    </html>
+  );
+}
